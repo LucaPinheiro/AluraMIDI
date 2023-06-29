01037a3a19ca671a91dec09c05f0cf30aa5aa9c2
LucaPinheiro <lucapgomes11@gmail.com>
2023-06-28 21:18:11 -0300

    Adicionando som às teclas

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
-function tocaSomPom() {
-  document.querySelector("#som_tecla_pom").play();
+function tocaSom(idElementoAudio) {
+  document.querySelector(idElementoAudio).play();
 }
 
-function tocaSomClap() {
-    document.querySelector("#som_tecla_clap").play();
-}
 
-var a = document.querySelector(".tecla_pom").onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while(contador < listaDeTeclas.length) {
+
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+
+  console.log(instrumento);
+
+  //template string 
+  const idAudio = `#som_${instrumento}`
+  
+  tecla.onclick = function() {
+    tocaSom(idAudio);
+  };
+
+  contador++;
+  console.log(contador);
+}
