031e70724d8f8ca564190b6e5f8b6c6aedca824a
LucaPinheiro <lucapgomes11@gmail.com>
2023-07-01 00:52:56 -0300

    Otimizando os cliques do usuário

--- main.js
+++ main.js
@@ -5,22 +5,14 @@ function tocaSom(idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
 
-while(contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
   const tecla = listaDeTeclas[contador];
   const instrumento = tecla.classList[1];
-
-  console.log(instrumento);
-
-  //template string 
   const idAudio = `#som_${instrumento}`
   
   tecla.onclick = function() {
     tocaSom(idAudio);
   };
-
-  contador++;
-  console.log(contador);
 }
